d85ecea51998cbc24b740d8fdbb74ff8148d98f6
KarthikBr97 <104575490+KarthikBr97@users.noreply.github.com>
2023-05-27 13:00:22 +0530

    Add files via upload

---
+++ src/main/java/com/java/pagination/StudentService.java
@@ -0,0 +1,32 @@
+package com.java.pagination;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+
+
+public class StudentService {
+	
+	@Autowired
+	private StudentRepository repository;
+	
+	public List<Student> findAllRecord(){
+		return repository.findAll();
+	}
+
+	public Page<Student> findRecordsWithPagination(int offset, int pageSize){
+		
+		return repository.findAll(PageRequest.of( offset, pageSize));
+	}
+	
+	public Page<Student> findRecordsWithPaginationAndSorting(int offset, int pageSize, String field){
+		
+		return repository.findAll(PageRequest.of( offset, pageSize).withSort(Sort.by(field)));
+		
+		
+	}
+}
